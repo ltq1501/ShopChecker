3a62cfc1cb6936ed176aa49c63795f42536cdb50
jL_1501 <quanlam139@gmail.com>
2025-06-01 21:54:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-// server.js
-import http from "http";
+// Gọi file bot chính để chạy bot Discord
+import './SkinPeek.js';
 
+import http from 'http';
 const PORT = process.env.PORT || 8080;
 
+// Tạo 1 HTTP server đơn giản để Render detect "bot is running"
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Bot is running!\n");
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running!\n');
 });
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`🌍 Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`🌍 HTTP server running on port ${PORT}`);
 });
